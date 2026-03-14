ab1074c3ec2873013f7a4a800ea34469a0d696a6
tathyadoot-png <tathyadoot@gmail.com>
2026-03-14 14:13:39 +0530

    aaa

--- src/components/layout/ScrollToTop.tsx
+++ src/components/layout/ScrollToTop.tsx
@@ -34,7 +34,7 @@ const ScrollToTop = () => {
           animate={{ opacity: 1, scale: 1, y: 0 }}
           exit={{ opacity: 0, scale: 0.5, y: 20 }}
           onClick={scrollToTop}
-          className="fixed bottom-8 md:right-8 right-4 z-[999] h-12 w-12 flex items-center justify-center rounded-full bg-green text-white shadow-[0_10px_20px_-5px_rgba(255,153,51,0.5)] border border-white/20 backdrop-blur-md transition-all hover:scale-110 active:scale-95 group"
+          className="fixed bottom-8 md:right-8 right-4 z-[999] h-12 w-12 flex items-center justify-center rounded-full bg-[#FFBF00] text-white shadow-[0_10px_20px_-5px_rgba(255,153,51,0.5)] border border-white/20 backdrop-blur-md transition-all hover:scale-110 active:scale-95 group"
         >
           <ChevronUp className="w-6 h-6 group-hover:-translate-y-1 transition-transform" />
         </motion.button>
